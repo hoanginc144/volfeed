7e20bceb8f15c2320d0cc4e2f3891419bb0d0486
hoanginc144 <hoanginc@gmail.com>
2025-04-08 15:55:05 +0700

    fix env name for deribit

--- api/deribit.py
+++ api/deribit.py
@@ -1,23 +1,24 @@
 """Deribit API client"""
-import os
-import logging
-from typing import Dict, Any
+
 import asyncio
+import logging
+import os
 from datetime import datetime
+from typing import Any, Dict
+
 import aiohttp
 from dotenv import load_dotenv
-
 
 # Load environment variables
 load_dotenv()
 
 
 class DeribitClient:
-    """Deribit API client """
+    """Deribit API client"""
 
     def __init__(self):
-        self.client_id = os.getenv("CLIENT_ID")
-        self.client_secret = os.getenv("CLIENT_SECRET")
+        self.client_id = os.getenv("DERIBIT_CLIENT_ID")
+        self.client_secret = os.getenv("DERIBIT_CLIENT_SECRET")
         self.session = aiohttp.ClientSession()
         self.access_token = None
         self.refresh_token = None
@@ -29,14 +30,17 @@ class DeribitClient:
         try:
             # Authenticate the client
             if not self.access_token:
-                url = 'https://www.deribit.com/api/v2/public/auth'
-                payload = {"grant_type": "client_credentials",
-                           "client_id": self.client_id, "client_secret": self.client_secret}
+                url = "https://www.deribit.com/api/v2/public/auth"
+                payload = {
+                    "grant_type": "client_credentials",
+                    "client_id": self.client_id,
+                    "client_secret": self.client_secret,
+                }
                 async with self.session.get(url, params=payload) as response:
                     res = await response.json()
-                    self.access_token = res['result']['access_token']
-                    self.refresh_token = res['result']['refresh_token']
-                    self.expires_in = res['result']['expires_in'] - 60
+                    self.access_token = res["result"]["access_token"]
+                    self.refresh_token = res["result"]["refresh_token"]
+                    self.expires_in = res["result"]["expires_in"] - 60
                     logging.info("Authenticated successfully")
         except aiohttp.ClientError as e:
             logging.error("Error authenticating the client: %s", e)
@@ -44,14 +48,16 @@ class DeribitClient:
     async def refresh_authentication(self) -> str:
         """Refresh the authentication token"""
         try:
-            url = 'https://www.deribit.com/api/v2/public/auth'
-            payload = {"grant_type": "refresh_token",
-                       "refresh_token": self.refresh_token}
+            url = "https://www.deribit.com/api/v2/public/auth"
+            payload = {
+                "grant_type": "refresh_token",
+                "refresh_token": self.refresh_token,
+            }
             async with self.session.get(url, params=payload) as response:
                 res = await response.json()
-                self.access_token = res['result']['access_token']
-                self.refresh_token = res['result']['refresh_token']
-                self.expires_in = res['result']['expires_in'] - 60
+                self.access_token = res["result"]["access_token"]
+                self.refresh_token = res["result"]["refresh_token"]
+                self.expires_in = res["result"]["expires_in"] - 60
                 logging.info("Authentication tokens refreshed")
         except aiohttp.ClientError as e:
             logging.error("Error refreshing the authentication token: %s", e)
@@ -59,19 +65,20 @@ class DeribitClient:
     async def get(self, end_point: str, payload: Dict[str, Any]) -> Dict[str, Any]:
         """Make a GET request to the Deribit API"""
         try:
-            url = f'https://www.deribit.com/api/v2/{end_point}'
+            url = f"https://www.deribit.com/api/v2/{end_point}"
             headers = {}
             if "private" in end_point:
                 # Check if the access token has expired
-                elapsed = (datetime.now() -
-                           self.tokens_last_updated).total_seconds()
+                elapsed = (datetime.now() - self.tokens_last_updated).total_seconds()
                 if elapsed >= self.expires_in:
                     await self.refresh_authentication()
                     self.tokens_last_updated = datetime.now()
                 headers = {"Authorization": f"Bearer {self.access_token}"}
-            async with self.session.get(url, headers=headers, params=payload) as response:
+            async with self.session.get(
+                url, headers=headers, params=payload
+            ) as response:
                 res = await response.json()
-                return res['result']
+                return res["result"]
         except aiohttp.ClientError as e:
             logging.error("Error making GET request to the Deribit API: %s", e)
 
@@ -93,7 +100,7 @@ async def main():
         print(start)
         while True:
             data = await deribit.get("private/get_account_summary", {"currency": "BTC"})
-            print(data['options_vega'])
+            print(data["options_vega"])
             end = datetime.now()
             elapsed = (end - start).total_seconds() / 60
             print(f"Elapsed time: {round(elapsed, 2)} minutes")
@@ -103,6 +110,7 @@ async def main():
     finally:
         await deribit.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     asyncio.run(main())
